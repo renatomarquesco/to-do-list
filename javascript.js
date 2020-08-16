80fe55cb59db72ca1908eb705ee1edbabf4a3fcc
renatomarquesco <67842526+renatomarquesco@users.noreply.github.com>
2020-08-16 22:11:22 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -19,7 +19,6 @@ $(document).ready(function () {
                 divToadd.empty()
 
                 // function to iterate over response and get all the tasks/
-
                 response.tasks.forEach(function (task) {
                     var newdiv = $("<div class ='row newrow col-12 d-flex newdiv '></div>")
                     divToadd.prepend(newdiv);
@@ -27,17 +26,22 @@ $(document).ready(function () {
                     newdiv.append(divCheck)
                     var span = $("<img class = 'span' src='./imgs/lista-de-controle.png' width = '40px' alt=''>")
                     divCheck.append(span);
-                    var checkbox = $('<input class= "check-box" type="checkbox" id="checkbox" data-id = "' + task.id + '"' + (task.completed ? 'checked' : "") + '>');
+                    var checkbox = $('<input class= "check-box" type="checkbox" id="checkbox" data-id = "' + task.id + '"' + (task.completed ? 'checked' : "") + '>')
+
                     divCheck.append(checkbox)
                     var divText = $("<div class ='col-lg-7 col-6 text-left div-text'></div>")
                     newdiv.append(divText);
-                    divText.append("<p class = 'text' data-id = '" + task.id + "'>" + task.content + "</p>")
+                    var taskText = $("<p class = 'text' data-id = '" + task.id + "'>" + task.content + "</p>")
+                    divText.append(taskText)
+
+                    if (task.completed) {
+                        taskText.css("text-decoration", "line-through");
+                    }
+
                     var divBtn = $("<div class ='col-lg-2 col-2 div-btn text-right'></div>")
                     newdiv.append(divBtn)
                     var removebtn = $("<img class = 'remove' src='./imgs/excluir-preto.png' width='12px' alt='' data-id =" + task.id + ">")
                     divBtn.append(removebtn);
-
-
                 })
             },
             error: function (request, textStatus, errorMessage) {
@@ -117,6 +121,7 @@ $(document).ready(function () {
             url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_active?api_key=178',
             dataType: "json",
             success: function (response, textStatus) {
+
                 displayTasks;
             },
 
@@ -126,15 +131,13 @@ $(document).ready(function () {
         })
     }
     // link active and completed functions to buttons
-    $(document).on("change", ".check-box", function () {
+    var checkboxes = $(document).on("change", ".check-box", function () {
         var dataId = $(this).data("id");
         if (this.checked) {
             markCompleted($(this).data("id"));
-            $(this).siblings().css("color", "#f1acac")
             $(this).parent().next().css("text-decoration", "line-through")
         } else {
             markActive($(this).data("id"))
-            $(this).siblings().css("color", "#5a5a5a")
             $(this).parent().next().css("text-decoration", "none")
 
         }
@@ -153,21 +156,21 @@ $(document).ready(function () {
                 divToadd.empty();
                 response.tasks.forEach(function (task) {
                     if (task.completed) {
-                    var newdiv = $("<div class ='row newrow col-12 d-flex newdiv '></div>")
-                    divToadd.prepend(newdiv);
-                    var divCheck = $("<div class ='col-lg-3 col-4 text-left div-check'></div>")
-                    newdiv.append(divCheck)
-                    var span = $("<img class = 'span' src='./imgs/lista-de-controle.png' width = '40px' alt=''>")
-                    divCheck.append(span);
-                    var checkbox = $('<input class= "check-box" type="checkbox" id="checkbox" data-id = "' + task.id + '"' + (task.completed ? 'checked' : "") + '>');
-                    divCheck.append(checkbox)
-                    var divText = $("<div class ='col-lg-7 col-6 text-left div-text'></div>")
-                    newdiv.append(divText);
-                    divText.append("<p class = 'text' data-id = '" + task.id + "'>" + task.content + "</p>")
-                    var divBtn = $("<div class ='col-lg-2 col-2 div-btn text-right'></div>")
-                    newdiv.append(divBtn)
-                    var removebtn = $("<img class = 'remove' src='./imgs/excluir-preto.png' width='12px' alt='' data-id =" + task.id + ">")
-                    divBtn.append(removebtn);
+                        var newdiv = $("<div class ='row newrow col-12 d-flex newdiv '></div>")
+                        divToadd.prepend(newdiv);
+                        var divCheck = $("<div class ='col-lg-3 col-4 text-left div-check'></div>")
+                        newdiv.append(divCheck)
+                        var span = $("<img class = 'span' src='./imgs/lista-de-controle.png' width = '40px' alt=''>")
+                        divCheck.append(span);
+                        var checkbox = $('<input class= "check-box" type="checkbox" id="checkbox" data-id = "' + task.id + '"' + (task.completed ? 'checked' : "") + '>');
+                        divCheck.append(checkbox)
+                        var divText = $("<div class ='col-lg-7 col-6 text-left div-text'></div>")
+                        newdiv.append(divText);
+                        divText.append("<p class = 'text' data-id = '" + task.id + "'>" + task.content + "</p>")
+                        var divBtn = $("<div class ='col-lg-2 col-2 div-btn text-right'></div>")
+                        newdiv.append(divBtn)
+                        var removebtn = $("<img class = 'remove' src='./imgs/excluir-preto.png' width='12px' alt='' data-id =" + task.id + ">")
+                        divBtn.append(removebtn);
                     }
                 })
 
@@ -187,20 +190,20 @@ $(document).ready(function () {
                 response.tasks.forEach(function (task) {
                     if (task.completed === false) {
                         var newdiv = $("<div class ='row newrow col-12 d-flex newdiv '></div>")
-                    divToadd.prepend(newdiv);
-                    var divCheck = $("<div class ='col-lg-3 col-4 text-left div-check'></div>")
-                    newdiv.append(divCheck)
-                    var span = $("<img class = 'span' src='./imgs/lista-de-controle.png' width = '40px' alt=''>")
-                    divCheck.append(span);
-                    var checkbox = $('<input class= "check-box" type="checkbox" id="checkbox" data-id = "' + task.id + '"' + (task.completed ? 'checked' : "") + '>');
-                    divCheck.append(checkbox)
-                    var divText = $("<div class ='col-lg-7 col-6 text-left div-text'></div>")
-                    newdiv.append(divText);
-                    divText.append("<p class = 'text' data-id = '" + task.id + "'>" + task.content + "</p>")
-                    var divBtn = $("<div class ='col-lg-2 col-2 div-btn text-right'></div>")
-                    newdiv.append(divBtn)
-                    var removebtn = $("<img class = 'remove' src='excluir-preto.png' width='12px' alt='' data-id =" + task.id + ">")
-                    divBtn.append(removebtn);
+                        divToadd.prepend(newdiv);
+                        var divCheck = $("<div class ='col-lg-3 col-4 text-left div-check'></div>")
+                        newdiv.append(divCheck)
+                        var span = $("<img class = 'span' src='./imgs/lista-de-controle.png' width = '40px' alt=''>")
+                        divCheck.append(span);
+                        var checkbox = $('<input class= "check-box" type="checkbox" id="checkbox" data-id = "' + task.id + '"' + (task.completed ? 'checked' : "") + '>');
+                        divCheck.append(checkbox)
+                        var divText = $("<div class ='col-lg-7 col-6 text-left div-text'></div>")
+                        newdiv.append(divText);
+                        divText.append("<p class = 'text' data-id = '" + task.id + "'>" + task.content + "</p>")
+                        var divBtn = $("<div class ='col-lg-2 col-2 div-btn text-right'></div>")
+                        newdiv.append(divBtn)
+                        var removebtn = $("<img class = 'remove' src='excluir-preto.png' width='12px' alt='' data-id =" + task.id + ">")
+                        divBtn.append(removebtn);
                     }
                 })
             }
